e345b05ab4c069a39c9faff9078c78d07770e347
mevaldezm <mevaldezm@gmail.com>
2019-11-25 18:52:10 -0400

    2 files modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,28 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-
-import { products } from '../products';
+//import { products } from '../products';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
-  products = products;
+export class ProductListComponent implements OnInit{
+  //products = products;
+  products;
+  constructor(
+    private productService: ProductService
+  ) { }
 
   share() {
     window.alert('The product has been shared!');
   }
   onNotify(){
     window.alert("You will be notified when the product goes on sale");
+  }
+   ngOnInit() {
+    this.products = this.productService.getProducts();
   }
 }
 
---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable(
+  {providedIn : 'root'}
+)
+export class ProductService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+  getProdcts() {
+    return this.http.get('http://127.0.0.1:8000/products/');
+  }
+}
